7c50e96beac796568af57d85ee1ea8c56d572a15
Yo Iwamoto <you.iwamoto918@gmail.com>
2023-03-19 21:38:25 +0900

    set eslint.ignoreDuringBuild true

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
   pageExtensions: ['page.tsx', 'api.ts'],
   swcMinify: true,
   output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = withInterceptStdout(nextConfig, (text) => (text.includes('Duplicate atom key') ? '' : text));
