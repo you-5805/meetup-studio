a9cf794036e1ebf9efa02a7813b9575d13d93f76
Yo Iwamoto <you.iwamoto918@gmail.com>
2023-05-28 16:00:13 +0900

    add cn from shadcn/ui

--- src/lib/cn.ts
+++ src/lib/cn.ts
@@ -1,1 +1,6 @@
-export { default as cn } from 'classnames';
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
